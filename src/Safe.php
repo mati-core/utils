aaa79a5b3427bea2a137e037362baf87fe684d16
Martin Olmr <info@martinolmr.cz>
2021-02-24 18:16:38 +0100

    update composer.json

--- src/Safe.php
+++ src/Safe.php
@@ -133,4 +133,38 @@ class Safe
 			: md5($path);
 	}
 
+	/**
+	 * Convert dirty haystack to scalar haystack. If object implements __toString(), it will be called automatically.
+	 *
+	 * @param mixed $haystack
+	 * @param bool $rewriteObjectsToString
+	 * @return mixed
+	 */
+	public static function strictScalarType($haystack, bool $rewriteObjectsToString = true)
+	{
+		if (\is_array($haystack)) {
+			$return = [];
+
+			foreach ($haystack as $key => $value) {
+				$return[$key] = self::strictScalarType($value, $rewriteObjectsToString);
+			}
+
+			return $return;
+		}
+
+		if (\is_scalar($haystack)) {
+			return $haystack;
+		}
+
+		if (\is_object($haystack)) {
+			if ($rewriteObjectsToString === true && method_exists($haystack, '__toString')) {
+				return (string) $haystack;
+			}
+
+			return get_class($haystack);
+		}
+
+		return $haystack;
+	}
+
 }
